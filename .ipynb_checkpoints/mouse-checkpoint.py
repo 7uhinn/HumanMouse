597843b8210489e2564d9d944187d0af95210c69
Tuhin <tuhin_sarkar@srmap.edu.in>
2020-03-30 19:58:28 +0530

    modulation performed

--- .ipynb_checkpoints/mouse-checkpoint.py
+++ .ipynb_checkpoints/mouse-checkpoint.py
@@ -11,10 +11,9 @@ mouse = Controller()
 app = wx.App(False)
 
 (camx,camy) = (320,240) #image resolution of the captured image
-(sx,sy) = wx.GetDisplaySize() #Screen resolution
-
 lB = np.array([86,80,40])
 uB = np.array([148,255,255]) #lower and upper bound of HSV values for blue (object color)
+(sx,sy) = wx.GetDisplaySize() #Screen resolution
 
 #instance of OpenCV VideoCapture through default camera
 cam = cv2.VideoCapture(0)
@@ -23,9 +22,10 @@ cam = cv2.VideoCapture(0)
 kO = np.ones((5,5)) #for Opening
 kC = np.ones((20,20)) #for Closing
 
-FingersClosed = 0 #to signify if the fingers were closed or not
+global FingersClosed #to signify if the fingers were closed or not
+FingersClosed = 0
 
-while True:
+def MorphologicalOperations(camx,camy,lB,uB):
     ret, img = cam.read() #frames of the video stored in 'img' (real-time)
     img = cv2.resize(img,(camx,camy)) #image size set to the image resolution
 
@@ -39,56 +39,75 @@ while True:
     maskF = maskC
     conts,h = cv2.findContours(maskF.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE) #Areas of interests
     
-    if(len(conts)==2): #if there are 2 fingers apart
-        if(FingersClosed==1): 
+    return conts, img
+
+def Traversal(FingersClosed,mouse,conts,img):
+    if(FingersClosed==1): 
             FingersClosed=0
             mouse.release(Button.left) #release left click (traversal mode)
-        x1,y1,w1,h1 = cv2.boundingRect(conts[0]) 
-        x2,y2,w2,h2 = cv2.boundingRect(conts[1]) #returns outscribed rectangle of the AOIs
-        cv2.rectangle(img,(x1,y1),(x1+w1,y1+h1),(255,0,0),2)
-        cv2.rectangle(img,(x2,y2),(x2+w2,y2+h2),(255,0,0),2) #draws the rectangle
+    x1,y1,w1,h1 = cv2.boundingRect(conts[0]) 
+    x2,y2,w2,h2 = cv2.boundingRect(conts[1]) #returns outscribed rectangle of the AOIs
+    cv2.rectangle(img,(x1,y1),(x1+w1,y1+h1),(255,0,0),2)
+    cv2.rectangle(img,(x2,y2),(x2+w2,y2+h2),(255,0,0),2) #draws the rectangle
         
-        cx1=x1+w1//2 #x coordinate of centre of 1st AOI
-        cy1=y1+h1//2 #y coordinate of centre of 1st AOI
-        cx2=x2+w2//2 #x coordinate of centre of 2nd AOI
-        cy2=y2+h2//2 #y coordinate of centre of 2nd AOI
+    cx1=x1+w1//2 #x coordinate of centre of 1st AOI
+    cy1=y1+h1//2 #y coordinate of centre of 1st AOI
+    cx2=x2+w2//2 #x coordinate of centre of 2nd AOI
+    cy2=y2+h2//2 #y coordinate of centre of 2nd AOI
         
-        cx=(cx1+cx2)//2 #x coordinate of centre of both AOIs
-        cy=(cy1+cy2)//2 #y coordinate of centre of both AOIs
+    cx=(cx1+cx2)//2 #x coordinate of centre of both AOIs
+    cy=(cy1+cy2)//2 #y coordinate of centre of both AOIs
         
-        cv2.line(img, (cx1,cy1),(cx2,cy2),(255,0,0),2) #line joining the 2 AOIs
-        cv2.circle(img, (cx,cy),2,(0,0,255),2) #circle in the middle
+    cv2.line(img, (cx1,cy1),(cx2,cy2),(255,0,0),2) #line joining the 2 AOIs
+    cv2.circle(img, (cx,cy),2,(0,0,255),2) #circle in the middle
         
-        pos = (sx-(cx*sx//camx), cy*sy//camy) #mouse positioning in screen relative to movement in image
-        mouse.position = pos 
-        while mouse.position != pos:
-            pass
+    pos = (sx-(cx*sx//camx), cy*sy//camy) #mouse positioning in screen relative to movement in image
+    mouse.position = pos
+    
+    while mouse.position != pos:
+        pass
+    
+    return FingersClosed
+    
+def LeftClick(FingersClosed,mouse,conts,img):
+    x,y,w,h = cv2.boundingRect(conts[0]) #returns outscribed rectangle of the AOI
+        
+    if(FingersClosed==0):
+        FingersClosed=1
+        mouse.press(Button.left) #perform left click
+
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2) #draws the rectangle
+        
+    cx=x+w//2 #x coordinate of centre of AOI
+    cy=y+h//2 #y coordinate of centre of AOI
+        
+    cv2.circle(img,(cx,cy),(w+h)//4,(0,0,255),2) #larger circle
+        
+    pos = (sx-(cx*sx//camx), cy*sy//camy) #mouse positioning in screen relative to movement in image
+    mouse.position = pos 
+    while mouse.position!=pos:
+        pass
+    
+    return FingersClosed
+
+def RightClick(conts,img,mouse):
+    x,y,w,h = cv2.boundingRect(conts[0]) #returns outscribed rectangle of the AOI
+        
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2) #draws the rectangle
+        
+    mouse.press(Button.right) 
+    
+while True:
+    conts, img = MorphologicalOperations(camx,camy,lB,uB)
+    
+    if(len(conts)==2): #if there are 2 fingers apart
+        FingersClosed = Traversal(FingersClosed,mouse,conts,img)
         
     elif(len(conts)==1): #if the fingers join to make one large AOI (fingers closed)
-        x,y,w,h = cv2.boundingRect(conts[0]) #returns outscribed rectangle of the AOI
-        
-        if(FingersClosed==0):
-            FingersClosed=1
-            mouse.press(Button.left) #perform left click (traversal mode)
-
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2) #draws the rectangle
-        
-        cx=x+w//2 #x coordinate of centre of AOI
-        cy=y+h//2 #y coordinate of centre of AOI
-        
-        cv2.circle(img,(cx,cy),(w+h)//4,(0,0,255),2) #larger circle
-        
-        pos = (sx-(cx*sx//camx), cy*sy//camy) #mouse positioning in screen relative to movement in image
-        mouse.position = pos 
-        while mouse.position!=pos:
-            pass
+        FingersClosed = LeftClick(FingersClosed,mouse,conts,img)
         
     elif(len(conts)==3): #if there are 3 fingers apart
-        x,y,w,h = cv2.boundingRect(conts[0]) #returns outscribed rectangle of the AOI
-        
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2) #draws the rectangle
-        
-        mouse.press(Button.right) 
+        RightClick(conts,img,mouse)
         
     cv2.imshow("cam",img) #finally show image on the 'cam' window
     cv2.waitKey(5)
